aaf2ad250275c8f1edda93f8eb9d9f33a499f039
s-koike <s-koike@techno-net.co.jp>
2026-04-14 19:35:34 +0900

    お知らせ機能のモデルが不足していたため追加

--- SCHearing.API/obj/Debug/net8.0/SCHearing.API.AssemblyInfo.cs
+++ SCHearing.API/obj/Debug/net8.0/SCHearing.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCHearing.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ee62467592e8c6952c90611610b217926a5230")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9908a6aefebca032080070fe9a7b0a8a63ab5ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCHearing.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCHearing.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
